238b08549263e969f988d77ba5b6c6161d1fa43a
Federico Zelarayan <115909271+fedezelarayan@users.noreply.github.com>
2023-07-10 12:53:28 -0300

    Delete Proyecto Individual - Doggies/PI-Food-main directory

--- Proyecto Individual - Doggies/PI-Food-main/api/src/routes/dietsRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { getAllDiets, obtenerTodasLasDietas } = require('../Handlers/dietsHandlers')
-
-const dietsRouter = Router();
-
-dietsRouter.get('/', getAllDiets)
-dietsRouter.get('/all', obtenerTodasLasDietas)
-
-module.exports = dietsRouter;
